693b50bfaae3b0d0cbc7bc11fade72ec10457823
Dmitriy Malych <malllkin007@gmail.com>
2016-10-16 21:08:05 +0300

    Init commit. Добавлен gitignore. Добавлен класс управления двигателем корабля. Начата реализация класса управления поворотами корабля.

---
+++ Assets/Engine.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class Engine : MonoBehaviour
+{
+    [SerializeField]
+    private float MaximumSpeed;
+    [SerializeField]
+    private float MinimumSpeed = 0f;
+    [SerializeField]
+    private float Acceleration;
+
+    private AccelerationDirection mCurrentAccelerationDirection;
+
+    public float CurrentSpeed
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+    /// Функция торможения, уменьшает значение текущей скорости.
+    /// </summary>
+    public void Brake()
+    {
+        mCurrentAccelerationDirection = AccelerationDirection.Backward;
+    }
+
+    /// <summary>
+    /// Функция ускорения, увеличивает значение текущей скорости.
+    /// </summary>
+    public void Accelerate()
+    {
+        mCurrentAccelerationDirection = AccelerationDirection.Forward;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        this.CurrentSpeed = 0f;
+        this.mCurrentAccelerationDirection = AccelerationDirection.Forward;
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCurrentSpeed();
+        ResetAcceleration();
+    }
+
+    private void ResetAcceleration()
+    {
+        mCurrentAccelerationDirection = AccelerationDirection.Hold;
+    }
+
+    private void UpdateCurrentSpeed()
+    {
+        this.CurrentSpeed += (int)mCurrentAccelerationDirection * Acceleration * Time.deltaTime;
+
+        if (CurrentSpeed > MaximumSpeed)
+        {
+            CurrentSpeed = MaximumSpeed;
+        }
+
+        if (CurrentSpeed < MinimumSpeed)
+        {
+            CurrentSpeed = MinimumSpeed;
+        }
+    }
+
+    internal enum AccelerationDirection
+    {
+        Forward = 1,
+        Hold = 0,
+        Backward = -1
+    }
+}
